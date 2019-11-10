4d01944b252dd2d3e67ed749c1577d99665b13d0
zhangwengao <zhangwengao@yy.com>
2019-11-10 17:01:45 +0800

    no message

---
+++ coordinatorLayout/src/main/java/com/android/coordinatorLayout/RunMain.java
@@ -0,0 +1,42 @@
+package com.android.coordinatorLayout;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Set;
+
+public class RunMain {
+    public static void main(String[] args) {
+        HashMap<Long, Integer> longLongHashMap = new HashMap<>();
+        HashMap<String, String> filter = new HashMap<>();
+        String filePath = "";
+        File file = new File(filePath);
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            for (File subFile : files) {
+                if(subFile.isFile()){
+                    long length = subFile.length();
+                    Integer aLong = longLongHashMap.get(length);
+                    if(aLong==null){
+                        longLongHashMap.put(length, 1);
+                    }else{
+                        longLongHashMap.put(length, aLong+1);
+
+                        DecimalFormat df = new DecimalFormat("#.00");
+                        String size = df.format((double) length / 1024) + "KB";
+
+                        filter.put(subFile.getName(),size);
+                    }
+                }
+            }
+            System.out.println(longLongHashMap.size()+"---------------------------------"+filter.size());
+            Set<String> strings = filter.keySet();
+            for (String key : strings) {
+                String value = filter.get(key);
+                System.out.println(key+"  ---  "+value);
+            }
+        }else{
+	        System.out.println("--------dir no exist-------------------------");
+        }
+    }
+}
---
+++ ioc/src/main/java/ioc/RunMain.java
@@ -0,0 +1,6 @@
+public class RunMain {
+    public static void main(String[] args) {
+        String result = "Run Main";
+        System.out.println(result);
+    }
+}
